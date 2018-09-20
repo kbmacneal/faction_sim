37a39b118681c4df8ffae06fcf8d97d29affbfbd
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-09-19 22:30:59 -0500

    more data

---
+++ Classes/cls_roll.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace faction_sim.Classes
+{
+
+    public class roller
+    {
+       public static List<int> RollKeeps(string base_roll, int keep_int)
+        {            
+            return Roll(base_roll).OrderBy(x=>x).Take(keep_int).ToList();
+        }
+        
+        public static List<int> Roll(string roll)
+        {
+            List<int> dice_results = new List<int>();
+
+            DiceBag db = new DiceBag();
+            char[] dice_splits = {'d','D'};
+            if (roll.Contains('-') || roll.Contains('+'))
+            {
+                char[] splits = new char[] { '+', '-' };
+                string[] two_parts = roll.Split(splits);
+                int mod = 0;
+                if (roll.Contains('-'))
+                {
+                    mod = -1 * Convert.ToInt32(two_parts[1]);
+                }
+                else
+                {
+                    mod = Convert.ToInt32(two_parts[1]);
+                }
+                string[] parameters = two_parts[0].Split(dice_splits);
+                uint num_dice = 1;
+                int dice_sides = 2;
+
+                if (parameters[0] != "")
+                {
+                    num_dice = Convert.ToUInt32(parameters[0]);
+                    dice_sides = Convert.ToInt32(parameters[1]);
+                }
+                else
+                {
+                    dice_sides = Convert.ToInt32(parameters[1]);
+                }
+
+                DiceBag.Dice dice = (DiceBag.Dice)System.Enum.Parse(typeof(DiceBag.Dice), dice_sides.ToString());
+
+                dice_results = db.RollQuantity(dice, num_dice);
+
+                dice_results.Add(mod);
+            }
+            else
+            {
+                string[] parameters = roll.Split(dice_splits);
+                uint num_dice = 1;
+                int dice_sides = 2;
+
+                if (parameters[0] != "")
+                {
+                    num_dice = Convert.ToUInt32(parameters[0]);
+                    dice_sides = Convert.ToInt32(parameters[1]);
+                }
+                else
+                {
+                    dice_sides = Convert.ToInt32(parameters[1]);
+                }
+
+                DiceBag.Dice dice = (DiceBag.Dice)System.Enum.Parse(typeof(DiceBag.Dice), dice_sides.ToString());
+
+                dice_results = db.RollQuantity(dice, num_dice);
+
+            }
+
+            return dice_results;
+
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using faction_sim.Classes;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace faction_sim
 {
     class Program
     {
+        public static Random rand = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<Classes.Factions.Faction, List<Classes.Assets.Asset>> members = new Dictionary<Classes.Factions.Faction, List<Classes.Assets.Asset>>();
+
+            List<Classes.Factions.Faction> combatants = initialize_factions(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]));
+
+            List<Classes.Assets.Asset> attacking_assets = initialize_assets(get_ids("1.txt").ToArray());
+
+            List<Classes.Assets.Asset> defending_assets = initialize_assets(get_ids("2.txt").ToArray());
+
+            members.Add(combatants[0],attacking_assets);
+
+            members.Add(combatants[0],defending_assets);
         }
+
+        private static List<int> get_ids (string path)
+        {
+            List<int> rtner = new List<int>();
+
+            string[] content = System.IO.File.ReadAllLines(path);
+
+            content.ToList().ForEach(e=>rtner.Add(Convert.ToInt32(e)));
+
+
+            return rtner;
+        }
+
+        private static List<Classes.Assets.Asset> initialize_assets (int[] ids)
+        {
+            List<Classes.Assets.Asset> rtner = new List<Classes.Assets.Asset>();
+
+            List<Classes.Assets.Asset> master_list = Classes.Assets.Asset.FromJson(System.IO.File.ReadAllText("assets.json")).ToList();
+
+            ids.ToList().ForEach(e=> rtner.Add(master_list.First(f=>f.Id == e)));
+
+            return rtner;
+        }
+
+
+        private static List<Classes.Factions.Faction> initialize_factions (int faction_atk, int faction_defend)
+        {
+            List<Classes.Factions.Faction> rtner = new List<Classes.Factions.Faction>();
+
+            List<Classes.Factions.Faction> master_list = Classes.Factions.Faction.FromJson(System.IO.File.ReadAllText("factions.json")).ToList();
+
+            rtner.Add(master_list.First(e=>e.Id == faction_atk));
+            rtner.Add(master_list.First(e=>e.Id == faction_defend));
+
+            return rtner;
+        }
+
     }
 }
