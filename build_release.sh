3bb80e13c4f84c195891eaa53c73ed4049e5c789
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-09-21 13:58:37 -0500

    adding build script

---
+++ build_release.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+rm -r -f bin
+
+dotnet publish --self-contained=yes -r win10-x64
+
+dotnet publish --self-contained=yes -r win10-x86
+
+dotnet publish --self-contained=yes -r linux-x64
+
+dotnet publish --self-contained=yes -r centos-x64
+
+dotnet publish --self-contained=yes -r ubuntu.18.04-x64
+
+dotnet publish --self-contained=yes -r ubuntu.16.04-x64
+
+dotnet publish --self-contained=yes -r osx-x64
+
+
+cd bin/Debug/netcoreapp2.1
+
+zip -r win10-x64.zip win10-x64/*
+zip -r win10-x86.zip win10-x86/*
+zip -r linux-x64.zip linux-x64/*
+zip -r centos-x64.zip centos-x64/*
+zip -r ubuntu.18.04-x64.zip ubuntu.18.04-x64/*
+zip -r ubuntu.16.04-x64.zip ubuntu.16.04-x64/*
+zip -r osx-x64.zip osx-x64/*
