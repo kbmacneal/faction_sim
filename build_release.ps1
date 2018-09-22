35d966c4298366f70a0da89b63248fc7d3b376bb
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-09-21 22:10:57 -0500

    adding powershell build script

---
+++ build_release.ps1
@@ -0,0 +1,28 @@
+rm -r -Force bin
+
+dotnet publish --self-contained=yes -r win10-x64
+
+dotnet publish --self-contained=yes -r win10-x86
+
+dotnet publish --self-contained=yes -r linux-x64
+
+dotnet publish --self-contained=yes -r centos-x64
+
+dotnet publish --self-contained=yes -r ubuntu.18.04-x64
+
+dotnet publish --self-contained=yes -r ubuntu.16.04-x64
+
+dotnet publish --self-contained=yes -r osx-x64
+
+
+cd bin/Debug/netcoreapp2.1
+
+Compress-Archive win10-x64 -DestinationPath win10-x64.zip
+Compress-Archive win10-x86 -DestinationPath win10-x86.zip
+Compress-Archive linux-x64 -DestinationPath linux-x64.zip
+Compress-Archive centos-x64 -DestinationPath centos-x64.zip
+Compress-Archive ubuntu.18.04-x64 -DestinationPath ubuntu.18.04-x64.zip
+Compress-Archive ubuntu.16.04-x64 -DestinationPath ubuntu.16.04-x64.zip
+Compress-Archive osx-x64 -DestinationPath osx-x64.zip
+
+cd $PSScriptRoot
