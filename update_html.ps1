364c747536130904b7e4b518ab590d38efe78d79
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-09-24 17:27:15 -0500

    updating property orders

--- update_html.ps1
+++ update_html.ps1
@@ -1,7 +1,7 @@
 $obj = get-content $PSScriptRoot/assets.json | convertfrom-json
 
-$obj | ConvertTo-Html -CssUri $PSScriptRoot/output.css | out-file $PSScriptRoot/assets.html
+$obj | Select-Object -Property ID,Name,HP,Attack,Counterattack,AttackStats,AttackDice,DefenderReroll,AttackerReroll | ConvertTo-Html -CssUri $PSScriptRoot/output.css | out-file $PSScriptRoot/assets.html
 
 $obj = get-content $PSScriptRoot/factions.json | ConvertFrom-Json
 
-$obj | ConvertTo-Html -CssUri $PSScriptRoot/output.css | out-file $PSScriptRoot/factions.html
+$obj | Select-Object ID,"Faction Name",Force,Cunning,Wealth,AttackerReroll,AttackerRerollStat,DefenderReroll,DefenderRerollStat,AttackerRerolled,DefenderRerolled,NumAttackerRerolls,NumDefenderRerolls,AlwaysRerollAtk,AlwaysRerollDef,PMax  | ConvertTo-Html -CssUri $PSScriptRoot/output.css | out-file $PSScriptRoot/factions.html
