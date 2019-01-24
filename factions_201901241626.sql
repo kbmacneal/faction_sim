77ae98595940db9b9d99e5129e2e9e8ec625a8be
Ken MacNeal <kenneth.b.macneal@gmail.com>
2019-01-24 16:27:19 -0600

    sql statement export

---
+++ factions_201901241626.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO factions ("Name","Force","Cunning","Wealth","Id","PMax",hp) VALUES 
+('House Crux','6','5','3',0,'False',29)
+,('House Fornax','5','3','6',1,'False',29)
+,('House Vela','3','6','5',2,'False',28)
+,('The Houses Minor','7','7','7',3,'False',12)
+,('"House" Vagrant','5','6','3',4,'False',29)
+,('14 Red Dogs Triad','3','6','5',5,'False',29)
+,('The Deathless','6','3','5',6,'False',29)
+,('The High Church of Messiah-as-Emperox','5','8','7',7,'True',49)
+,('The Church of Humanity, Repentant','3','6','5',8,'False',29)
+,('The Prism Network','3','6','5',9,'False',26)
+;
+INSERT INTO factions ("Name","Force","Cunning","Wealth","Id","PMax",hp) VALUES 
+('The Unified Peoples Collective','5','6','3',10,'False',29)
+,('ACRE','7','5','8',11,'False',49)
+,('The Trilliant Ring','3','5','6',12,'False',29)
+;
