2ba3c38c9c62424a440779b5c24cf17f7364eef8
Ken MacNeal <kenneth.b.macneal@gmail.com>
2019-01-24 13:45:52 -0600

    removing old files

--- update_html.ps1
+++
@@ -1,7 +0,0 @@
-$obj = get-content $PSScriptRoot/assets.json | convertfrom-json
-
-$obj | Select-Object -Property ID,Name,Description,HP,Attack,Counterattack,AttackStats,AttackDice,DefenderReroll,AttackerReroll | ConvertTo-Html -CssUri $PSScriptRoot/output.css | out-file $PSScriptRoot/assets.html
-
-$obj = get-content $PSScriptRoot/factions.json | ConvertFrom-Json
-
-$obj | Select-Object ID,"Faction Name",Force,Cunning,Wealth,PMax  | ConvertTo-Html -CssUri $PSScriptRoot/output.css | out-file $PSScriptRoot/factions.html
